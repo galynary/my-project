b2b2c19d06fbc139e800e93b35b74b2ee7374c15
galynary <galyna.rybak.789@gmail.com>
2024-12-17 23:41:44 +0200

    add img

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { SharedLayout } from './components/SharedLayout/SharedLayout';
 import { Home } from './components/Home/Home';
-import './styles/index.js';
-import './styles/variables.css';
 
 export const App = () => {
   return (
